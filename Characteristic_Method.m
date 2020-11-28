dede42bd7e363dd836f9ebfc23b586fe89824adb
Petires <Petires@wp.pl>
2020-11-28 12:19:16 +0000

    Add files via upload
    Fixed PMF

--- Characteristic_Method.m
+++ Characteristic_Method.m
@@ -1,8 +1,8 @@
 M = 2.6
 y = 1.14
 n = 10
-nu = sqrt((y+1)/(y-1))*atan((y-1)/(y+1)*(M^2-1)) - atan(M^2-1)
-theta_max = nu/2
+nu = 0.5*(sqrt((y+1)/(y-1))*atan(sqrt((y-1)/(y+1)*(M^2-1))) - atan(sqrt(M^2-1)))
+theta_max = nu
 dtheta = theta_max/n
 theta = [dtheta:dtheta:theta_max]
 nu = theta
@@ -17,8 +17,8 @@ Mach = zeros(n+1,n)
 
 for i = 1:n
     for j = 1:(n+1)
-        Mach(j,i)=fzero(@(x)sqrt((y+1)/(y-1))*atan(sqrt((y-1)/(y+1))*(x^2-1))-atan(x^2-1) - Nu(j,i),1);
+        Mach(j,i)=fzero(@(x)(sqrt((y+1)/(y-1))*atan(sqrt((y-1)/(y+1)*(x^2-1)))-atan(sqrt(x^2-1))) - Nu(j,i),2);
     end
 end
-
+Mach
 Mu = asin(1./Mach)

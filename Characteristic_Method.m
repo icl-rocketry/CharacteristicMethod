8dd0477ba2ab8c2032f85390b5da097e09833421
Petires <Petires@wp.pl>
2020-12-05 03:21:26 +0000

    Add files via upload
    Final version of code

--- Characteristic_Method.m
+++ Characteristic_Method.m
@@ -1,6 +1,6 @@
 M = 2.6
 y = 1.14
-n = 10
+n = 39
 nu = 0.5*(sqrt((y+1)/(y-1))*atan(sqrt((y-1)/(y+1)*(M^2-1))) - atan(sqrt(M^2-1)))
 theta_max = nu
 dtheta = theta_max/n
@@ -41,10 +41,10 @@ for i=1:n
             end
         else
             if i == j
-                x(i,j) = x(i-1,j) - y(i-1,j)/dydx_n(i,j)
+                x(i,j) = x(i-1,j) - y(i-1,j)/dydx_n(i,j);
             else
-                x(i,j) = (y(i,j-1) - y(i-1,j) - x(i,j-1)*dydx_p(i,j) + x(i-1,j)*dydx_n(i,j))/(dydx_n(i,j)-dydx_p(i,j))
-                y(i,j) = y(i-1,j) + dydx_n(i,j)*(x(i,j)-x(i-1,j))
+                x(i,j) = (y(i,j-1) - y(i-1,j) - x(i,j-1)*dydx_p(i,j) + x(i-1,j)*dydx_n(i,j))/(dydx_n(i,j)-dydx_p(i,j));
+                y(i,j) = y(i-1,j) + dydx_n(i,j)*(x(i,j)-x(i-1,j));
             end
         end
     end
@@ -55,3 +55,18 @@ for i = 1:n
 plot(x(i,i:n),y(i,i:n))
 hold on
 end
+theta_av = 0.5*(Theta(1:(n),n) + Theta(2:(n+1),n))
+a_w = tan(theta_av)
+a_c = dydx_p(1:n,n)
+X_c = x(1:n,n)
+Y_c = y(1:n,n)
+X_w = zeros(n+1,1)
+Y_w = [1;zeros(n,1)]
+for i=2:(n+1)
+    X_w(i,1) = (Y_w(i-1,1)-Y_c(i-1,1)-a_w(i-1,1)*X_w(i-1,1)+a_c(i-1,1)*X_c(i-1,1))/(a_c(i-1,1)-a_w(i-1,1));
+    Y_w(i,1) = Y_w(i-1,1) + a_w(i-1,1)*(X_w(i,1)-X_w(i-1,1));
+end 
+plot(X_w,Y_w)
+X_w
+X_c
+hold off
